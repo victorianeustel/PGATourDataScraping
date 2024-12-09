3a54a2c2232b942f53ebe8cb99b9622a1a17ec72
victoria.neustel <100964778+victorianeustel@users.noreply.github.com>
2024-12-09 17:21:18 -0600

    Checking ifcsvs are empty

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,6 +1,7 @@
 from .PathBuilder import *
 from .JSONDataMapping import *
 from .FileHelper import *
+from .CSVHelper import *
 
 print(f"Name: {__name__}")
 print(f"Package: {__package__}")
--- src/main.py
+++ src/main.py
@@ -2,9 +2,27 @@ from DownloadStatsCsvs import GetStatCsvs
 from Years import GetYears
 from classes.Year import Year
 from helpers.JSONDataMapping import *
+from helpers.CSVHelper import *
+import os
 
 years = GetYears()
 mapped_years = [Year(**y) for y in years]
 
-for year in mapped_years:
-    GetStatCsvs(year.year)
+# for year in mapped_years:
+#     GetStatCsvs(year.year)
+    
+path = os.getcwd() + '/data'
+
+print(path)
+
+# result = IsEmptyCSV('/Users/victorianeustel/Documents/GitHub/PGATourDataScraping/data/2013/money_finishes/finishes/top_10_finishes.csv')
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        if file.endswith(".csv"):
+            fullPath = os.path.join(subdir, file)
+            isEmpty = IsEmptyCSV(os.path.join(subdir, file))
+            if isEmpty == True:
+                print(' '.join([subdir, file, str(isEmpty)]))
+            # if isEmpty == True:
+            #     print(os.path.join(subdir, file))
+
