2171108239e743f9cc93f05fe9a00336e7743892
victoria.neustel <100964778+victorianeustel@users.noreply.github.com>
2024-12-09 08:50:37 -0600

    Adding functionanlity for data by years

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from DownloadStatsCsvs import GetStatCsvs
+from Years import GetYears
+from classes.Year import Year
+from helpers.JSONDataMapping import *
+
+years = GetYears()
+mapped_years = [Year(**y) for y in years]
+
+for year in mapped_years:
+    GetStatCsvs(str(year.year))
