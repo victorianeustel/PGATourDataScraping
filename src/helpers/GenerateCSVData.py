53eb5938aa88e2493a506727c26a1e8f3b431084
victoria.neustel <100964778+victorianeustel@users.noreply.github.com>
2024-12-13 20:09:04 -0600

    adding more endpoints for tournaments / schedule

--- src/helpers/GenerateCSVData.py
+++ src/helpers/GenerateCSVData.py
@@ -8,8 +8,12 @@ def CreateDirectory(path):
     os.mkdir(path)
     
 
-def CreateOrAppendCSV(path:str, fileName: str, fileWritingType: str = "w", 
-              header: list = None, content_rows: list = None):
+def CreateOrAppendCSV(path:str, 
+    fileName: str, 
+    fileWritingType: str = "w", 
+    header: list = None, 
+    content_rows: list = None):
+    
     filePath = '/'.join([path, fileName])
     with open(filePath, fileWritingType, newline='\n') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
