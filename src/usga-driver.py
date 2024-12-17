d00dae2fc3c6e37acf4395ac2e6c4fef66907945
victoria.neustel <100964778+victorianeustel@users.noreply.github.com>
2024-12-16 19:03:47 -0600

    data pulled update

--- src/usga-driver.py
+++ src/usga-driver.py
@@ -31,52 +31,55 @@ state_select = Select(mydriver.find_element(By.ID, 'ddlStates'))
 state_select_options = [state.get_attribute('value') for state in state_select.options]
 
 print(state_select_options)
-state_select.select_by_value('US-AL')
 
-# Click submit form
-ele = mydriver.find_element(By.ID, 'myButton')
-mydriver.execute_script("arguments[0].click();", ele)
+for state in state_select_options[1:len(state_select_options)]:
+    print(state)
+    state_select.select_by_value(state)
 
-# Wait for datatable result to load 
-time.sleep(2)
+    # Click submit form
+    ele = mydriver.find_element(By.ID, 'myButton')
+    mydriver.execute_script("arguments[0].click();", ele)
 
-# Updated current selection filter to 100 from 10
-filter_len_select = mydriver.find_element(By.XPATH, "//div[@id='resultTable_length']//select[@name='resultTable_length']")
-filter_select = Select(filter_len_select)
-filter_select.select_by_value('100')
+    # Wait for datatable result to load 
+    time.sleep(2)
 
-def pull_table_data(rows):
-    for row in rows:
-        cols = row.find_elements(By.TAG_NAME, "td")
-        facility_name = cols[0].text
-        
-        course_name = cols[1].text
-        a_ele = cols[1].find_element(By.TAG_NAME, "a")
-        course_detail_path = a_ele.get_attribute('href')
-        
-        course_id = int(course_detail_path.split('https://ncrdb.usga.org/courseTeeInfo?CourseID=')[1])
-        
-        city = cols[2].text
-        state = cols[3].text
-        
-        course = USGA_CourseSummary(course_id, facility_name, course_name, city, state, course_detail_path)
-        course.AddToCSV()
+    # Updated current selection filter to 100 from 10
+    filter_len_select = mydriver.find_element(By.XPATH, "//div[@id='resultTable_length']//select[@name='resultTable_length']")
+    filter_select = Select(filter_len_select)
+    filter_select.select_by_value('100')
 
-table_next_button = mydriver.find_element(By.ID, 'resultTable_next')
+    def pull_table_data(rows):
+        for row in rows:
+            cols = row.find_elements(By.TAG_NAME, "td")
+            facility_name = cols[0].text
+            
+            course_name = cols[1].text
+            a_ele = cols[1].find_element(By.TAG_NAME, "a")
+            course_detail_path = a_ele.get_attribute('href')
+            
+            course_id = int(course_detail_path.split('https://ncrdb.usga.org/courseTeeInfo?CourseID=')[1])
+            
+            city = cols[2].text
+            state = cols[3].text
+            
+            course = USGA_CourseSummary(course_id, facility_name, course_name, city, state, course_detail_path)
+            course.AddToCSV()
 
-# Get result table
-while True:
-    table = mydriver.find_element(By.ID, 'resultTable')
-    tbody = table.find_element(By.TAG_NAME, "tbody")
-    rows = tbody.find_elements(By.TAG_NAME, "tr")
-    pull_table_data(rows)
-
-    time.sleep(1)
-    
     table_next_button = mydriver.find_element(By.ID, 'resultTable_next')
-    if ('disabled' in table_next_button.get_attribute('class').split()):
-        break
-    
-    mydriver.execute_script("arguments[0].click();", table_next_button)
+
+    # Get result table
+    while True:
+        table = mydriver.find_element(By.ID, 'resultTable')
+        tbody = table.find_element(By.TAG_NAME, "tbody")
+        rows = tbody.find_elements(By.TAG_NAME, "tr")
+        pull_table_data(rows)
+
+        time.sleep(1)
+        
+        table_next_button = mydriver.find_element(By.ID, 'resultTable_next')
+        if ('disabled' in table_next_button.get_attribute('class').split()):
+            break
+        
+        mydriver.execute_script("arguments[0].click();", table_next_button)
 
 
